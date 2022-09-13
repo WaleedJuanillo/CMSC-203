acc2748574ece156cb2a61645872c26bd08b6538
WaleedJuanillo <112452406+WaleedJuanillo@users.noreply.github.com>
2022-09-13 18:33:36 +0800

    Add files via upload

---
+++ SentenceBuilderChallenge/ITEC105BuilderSentence.java
@@ -0,0 +1,15 @@
+public class ITEC105BuilderSentence {
+    public static void main (String args[])
+    {
+        String name = "Waleed Juanillo.";
+        int age = 19;
+        float grade = 1.69f;
+        String bloodtype = "AB+";
+
+        System.out.println("Hi, my name is " + name);
+        System.out.println("I am " + age + " years old. ");
+        System.out.println("My GPA is " + grade);
+        System.out.println("My blood type is " + bloodtype);
+
+    }
+}
